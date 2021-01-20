85864b7fabaa78918622740e28fe09e5a24814e6
rakshita.panchal <rakshita.panchal@brainvire.com>
2021-01-20 16:27:27 +0530

    remove unused code

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,9 +1,4 @@
 
-// ---------------- constants ---------------------
-// export const TEMP_ARRAY_1 = ["A", "B", "C"];
-// export const TEMP_ARRAY_2 = ["a", "b", "c"];
-// export const TEMP_ARRAY_3 = ["1", "2", "3"];
-
 export const NASA_API_KEY = '50o2RdKoN0J40a2fBIZ8VMeTabOej5vAnU2c5MqI'
 export const NASA_DEMO_KEY = 'DEMO_KEY'
 export const BASE_URL = 'https://api.nasa.gov/neo/rest/v1/neo'
